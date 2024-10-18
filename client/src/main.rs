93d995143a8bc05c04dec31408fe0fa482198cd3
CodingWithJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2024-10-18 17:28:35 -0400

    Adding configs

--- client/src/main.rs
+++ client/src/main.rs
@@ -11,21 +11,25 @@ struct GreetTimer(Timer);
 
 pub struct HelloPlugin;
 
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut App) {
+impl Plugin for HelloPlugin
+{
+    fn build(&self, app: &mut App)
+    {
         app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)));
         app.add_systems(Startup, add_people);
         app.add_systems(Update, (update_people, greet_people).chain());
     }
 }
 
-fn add_people(mut commands: Commands) {
+fn add_people(mut commands: Commands)
+{
     commands.spawn((Person, Name("Elaina Proctor".to_string())));
     commands.spawn((Person, Name("Renzo Hume".to_string())));
     commands.spawn((Person, Name("Zayna Nieves".to_string())));
 }
 
-fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>)
+{
     // update our timer with the time elapsed since the last update
     // if that caused the timer to finish, we say hello to everyone
     if timer.0.tick(time.delta()).just_finished() {
@@ -35,7 +39,8 @@ fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Na
     }
 }
 
-fn update_people(mut query: Query<&mut Name, With<Person>>) {
+fn update_people(mut query: Query<&mut Name, With<Person>>)
+{
     for mut name in &mut query {
         if name.0 == "Elaina Proctor" {
             name.0 = "Elaina Hume".to_string();
@@ -44,9 +49,7 @@ fn update_people(mut query: Query<&mut Name, With<Person>>) {
     }
 }
 
-fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_plugins(HelloPlugin)
-        .run();
+fn main()
+{
+    App::new().add_plugins(DefaultPlugins).add_plugins(HelloPlugin).run();
 }
